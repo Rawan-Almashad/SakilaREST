b9c944b222870638a7dfc66ed4b623b13f2a8d01
Rawan <rawanalmashad@gmail.com>
2026-04-24 18:00:18 +0300

    Modify ServiceFactory

--- src/main/java/org/iti/rest/factory/ServiceFactory.java
+++ src/main/java/org/iti/rest/factory/ServiceFactory.java
@@ -13,7 +13,15 @@ public class ServiceFactory {
     private static final CountryService countryService =
             new CountryService(new CountryDao());
     private static final CityService cityService =
-            new CityService(new CityDao(),countryService);
+            new CityService(new CityDao());
+    private static final StoreService storeService =
+            new StoreService(new StoreDao());
+    private static final StaffService staffService =
+            new StaffService(new StaffDao());
+    private static final CategoryService categoryService =
+            new CategoryService(new CategoryDao());
+    private static final CustomerService customerService =
+            new CustomerService(new CustomerDao());
 
     public static LanguageService getLanguageService() {
         return languageService;
@@ -24,4 +32,9 @@ public class ServiceFactory {
     public static ActorService getActorService(){return actorService;}
     public static CountryService getCountryService(){return countryService;}
     public static CityService getCityService(){return cityService;}
+    public static StoreService getStoreService(){return storeService;}
+    public static StaffService getStaffService(){return staffService;}
+    public static CategoryService getCategoryService(){return categoryService;}
+    public static CustomerService getCustomerService(){return customerService;}
+
 }

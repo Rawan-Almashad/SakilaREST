69375434549bcf545400e04e663bc6aa81156e79
Rawan <rawanalmashad@gmail.com>
2026-04-24 17:49:42 +0300

    Add Category DTOs

---
+++ src/main/java/org/iti/rest/dto/UpdateCategoryRequest.java
@@ -0,0 +1,22 @@
+package org.iti.rest.dto;
+
+public class UpdateCategoryRequest {
+    private Short categoryId;
+    private String name;
+
+    public Short getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Short categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
